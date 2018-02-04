02bb7419add08753e26b5d9d67c8c3bcae386e7f
E-DineshKumar <dineshkumar.e20@gmail.com>
2018-02-04 15:26:49 +0530

    Added new Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   visible = false;
-
+  message : String;
+  time = new Date(2018,1,20);
+  messages_receiver = ["How can I help you?"];
+  messages_sender = [] ;
+  
   onVisible(){
     this.visible = !this.visible;
+  }
+  send(){
+    if((this.message.length)!=0)
+    this.messages_sender.push(this.message);
   }
 }
